b86e36d50d6aaa6d7fd5733dbd725c13c3f1aa7b
maserati12 <triggamaserati@gmail.com>
2020-02-16 00:15:14 +0300

    drop, dragstart and dragover events

--- drag-and-drop.js
+++ drag-and-drop.js
@@ -1,10 +1,11 @@
 // DOM Elements
+const selectColors = document.querySelector('.select-colors');
 const pickColor = document.querySelectorAll('.pick-color');
 const selectedColors = document.querySelector('.selected-colors');
 
 pickColor.forEach(color => {
 	color.addEventListener('dragstart', (e) => {
-		e.dataTransfer.setData('text', [e.target.style.backgroundImage])
+		e.dataTransfer.setData('text', e.target.style.backgroundImage)
 	})
 })
 
@@ -16,7 +17,19 @@ selectedColors.addEventListener('drop', (e) => {
 	e.preventDefault()
 	let newColor = document.createElement('div');
 	newColor.setAttribute('class', 'picked-color');
+	newColor.setAttribute('draggable', 'true');
+	newColor.innerText = 'drag to remove';
 	let selectedColor = e.dataTransfer.getData('text');
 	newColor.style.backgroundImage = selectedColor;
 	selectedColors.appendChild(newColor);
+	// Show used color
+	pickColor.forEach(color => {
+		let txt = document.createElement('div');
+		txt.setAttribute('class', 'confirm');
+		txt.innerText = 'used';
+		const appendColor = () => {
+			color.style.backgroundImage == selectedColor ? color.appendChild(txt): 0;
+		}
+		color.children.length == 0 ? appendColor(): 0;
+	})
 })
